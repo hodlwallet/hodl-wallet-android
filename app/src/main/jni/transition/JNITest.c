a7b6f9cbce83976789f312de026231c323b9b17d
Igor Guerrero <igorgue@protonmail.com>
2018-08-07 15:55:13 -0400

    Fix transition JNI

--- app/src/main/jni/transition/JNITest.c
+++ app/src/main/jni/transition/JNITest.c
@@ -1,4 +1,16 @@
 //
-// Created by igor on 8/7/18.
+// Created by Igor Guerrero on 8/7/18.
 //
 
+#include <jni.h>
+#include <BRTest.h>
+
+#include "JNITest.h"
+
+JNIEXPORT jboolean JNICALL Java_com_jniwrappers_BRTest_BRCoreTests(
+    JNIEnv *env,
+    jobject thiz) {
+    jboolean r = (jboolean) BRCoreTests();
+
+    return r;
+}
--- app/src/main/jni/transition/JNITest.h
+++ app/src/main/jni/transition/JNITest.h
@@ -2,3 +2,20 @@
 // Created by igor on 8/7/18.
 //
 
+
+#ifndef BREADWALLET_JNITEST_H
+#define BREADWALLET_JNITEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <jni.h>
+
+JNIEXPORT jint JNICALL Java_com_jniwrappers_BRTest_BRCoreTests();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //BREADWALLET_JNITEST_H
