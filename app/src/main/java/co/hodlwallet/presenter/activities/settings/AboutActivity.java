9535e6a558ef67152a5f43f8a10925d7f64e4ca2
arubinofaux <arubinofaux@gmail.com>
2019-01-23 15:41:43 -0500

    update domain to https://hodlwallet.com

--- app/src/main/java/co/hodlwallet/presenter/activities/settings/AboutActivity.java
+++ app/src/main/java/co/hodlwallet/presenter/activities/settings/AboutActivity.java
@@ -80,7 +80,7 @@ public class AboutActivity extends BRActivity {
         blogShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hodlwallet.co"));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hodlwallet.com"));
                 startActivity(browserIntent);
                 app.overridePendingTransition(R.anim.enter_from_bottom, R.anim.empty_300);
             }
@@ -88,7 +88,7 @@ public class AboutActivity extends BRActivity {
         policyText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hodlwallet.co/privacy-policy"));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hodlwallet.com/privacy-policy"));
                 startActivity(browserIntent);
                 app.overridePendingTransition(R.anim.enter_from_bottom, R.anim.empty_300);
             }
